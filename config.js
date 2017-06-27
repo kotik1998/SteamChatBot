e750b003e7308a1038eeb8d76ce106e795408aef
Qasde <Qasde@users.noreply.github.com>
2017-06-27 20:31:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@
     
     owner: '', // Steam ID 64-bit integer
     domain: "", // Only used to request a new key if you don't have a one already. Update to your own domain.
-    group: '', // Group ID 64-bit integer, used for inviting people to your group //103582791458084036
+    group: '103582791458084036', // Group ID 64-bit integer, used for inviting people to your group //103582791458084036
     games: [
         
     ],
@@ -21,9 +21,9 @@
         '',
         ''
     ],
-    cleverbot: {
-        user: '',
-        key: ''
+    cleverbot: {                                                                
+        user: 'BXHCf5EtE2sLaPB8',                                               
+        key: 'mw50zghuNSCrCwezQG06uSOYV4pkhLyn'                                 
     },
     chatTime: 259200 // time in seconds it takes when you dont recieve any commands to leave chat
 };
